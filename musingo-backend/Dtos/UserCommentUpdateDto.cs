28e65d1e24bb49509aca26023b0eecfdbfdbef7d
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2022-03-25 19:58:31 +0100

    comment validation

--- musingo-backend/Dtos/UserCommentUpdateDto.cs
+++ musingo-backend/Dtos/UserCommentUpdateDto.cs
@@ -1,9 +1,13 @@
-﻿namespace musingo_backend.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace musingo_backend.Dtos
 {
     public class UserCommentUpdateDto
     {
         public int Id { get; set; }
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
         public double? Rating { get; set; }
+        [MaxLength(300, ErrorMessage = "Max length 300 characters")]
         public string? CommentText { get; set; }
     }
 }
