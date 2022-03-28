115425794f283b7ae94f630b93e5254d0d91f1a8
Maciej Mendel <maciej.mendel17@gmail.com>
2022-03-28 16:17:07 +0200

    added user details - as user profile

---
+++ musingo-backend/Dtos/UserDetailsDto.cs
@@ -0,0 +1,23 @@
+namespace musingo_backend.Dtos
+{
+    public class UserDetailsDto
+    {
+        // dane ktore widzi tylko uzytkownik
+        public string Name { get; set; }
+        public string? Surname { get; set; }
+        public string Email { get; set; }
+        public string? ImageUrl { get; set; }
+        public double AvgRating { get; set; }
+
+        // dane do wysylki
+        public string? City { get; set; }
+        public string? Street { get; set; }
+        public string? HouseNumber { get; set; }
+        public string? PostCode { get; set; }
+
+        // dane opcjonalne
+        public string? Gender { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime? Birth { get; set; }
+    }
+}
