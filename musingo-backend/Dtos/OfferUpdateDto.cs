b9be03d735e4b77414bc1c22c75f240cf64758c9
rtrydev <r.kulka44@gmail.com>
2022-04-06 10:12:25 +0200

    add dto for updating

---
+++ musingo-backend/Dtos/OfferUpdateDto.cs
@@ -0,0 +1,11 @@
+namespace musingo_backend.Dtos;
+
+public class OfferUpdateDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string ImageUrl { get; set; }
+    public double Cost { get; set; }
+    public string OfferStatus { get; set; }
+    public string ItemCategory { get; set; }
+}
