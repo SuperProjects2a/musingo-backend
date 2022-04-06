dfd7eb271ab855c4b4cf86fa3b744a6e27234bce
rtrydev <r.kulka44@gmail.com>
2022-04-06 14:15:15 +0200

    add validation for creating and updating offers

--- musingo-backend/Dtos/OfferCreateDto.cs
+++ musingo-backend/Dtos/OfferCreateDto.cs
@@ -1,9 +1,15 @@
-﻿namespace musingo_backend.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace musingo_backend.Dtos
 {
     public class OfferCreateDto
     {
+        [MaxLength(50)]
+        [MinLength(5)]
         public string Title { get; set; }
+        [MaxLength(9000)]
         public string Description { get; set; }
+        [Range(0d, 1000000d)]
         public double Cost { get; set; }
         public string ItemCategory { get; set; }
     }
--- musingo-backend/Dtos/OfferUpdateDto.cs
+++ musingo-backend/Dtos/OfferUpdateDto.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace musingo_backend.Dtos;
 
 public class OfferUpdateDto
 {
     public int Id { get; set; }
+    [MaxLength(50)]
+    [MinLength(5)]
     public string Title { get; set; }
+    [MaxLength(200)]
     public string? ImageUrl { get; set; }
+    [Range(0d, 1000000d)]
     public double Cost { get; set; }
     public string OfferStatus { get; set; }
     public string ItemCategory { get; set; }
+    [MaxLength(9000)]
     public string Description { get; set; }
 }
