2a169b8bdfbc6e0c01f2b4079575b9797ffddd54
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2022-04-12 21:19:48 +0200

    Update UserDto

--- musingo-backend/Dtos/UserDto.cs
+++ musingo-backend/Dtos/UserDto.cs
@@ -1,3 +1,5 @@
+using musingo_backend.Models;
+
 namespace musingo_backend.Dtos;
 
 public class UserDto
@@ -9,5 +11,6 @@ public class UserDto
     public string? ImageUrl { get; set; }
     public double AvgRating { get; set; }
     public string? PhoneNumber { get; set; }
+    public string? Role { get; set; }
 
 }
