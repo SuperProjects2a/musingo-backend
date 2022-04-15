12d6ea6e7806b377b996886c7047dfb819f3e38b
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2022-04-15 12:53:52 +0200

    Trying cqrs

--- musingo-backend/Controllers/OfferController.cs
+++ musingo-backend/Controllers/OfferController.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using musingo_backend.Authentication;
 using musingo_backend.Dtos;
 using musingo_backend.Models;
+using musingo_backend.Queries;
 using musingo_backend.Repositories;
 
 namespace musingo_backend.Controllers
@@ -15,20 +17,31 @@ namespace musingo_backend.Controllers
         private IMapper _mapper;
         private IOfferRepository _offerRepository;
         private IUserRepository _userRepository;
+        private IMediator _mediator;
         private IJwtAuth _jwtAuth;
 
-        public OfferController(IMapper mapper, IOfferRepository offerRepository, IJwtAuth jwt, IUserRepository userRepository)
+        public OfferController(IMapper mapper, IOfferRepository offerRepository, IJwtAuth jwt, IUserRepository userRepository, IMediator mediator)
         {
             _mapper = mapper;
             _offerRepository = offerRepository;
             _jwtAuth = jwt;
             _userRepository = userRepository;
+            _mediator = mediator;
         }
         [HttpGet]
         public async Task<ActionResult<ICollection<OfferDetailsDto>>> GetAll([FromQuery] OfferFilterDto filterDto)
         {
-            var offers = await _offerRepository.GetOfferByFilter(filterDto);
-            return Ok(_mapper.Map<ICollection<OfferDetailsDto>>(offers));
+            var request = new GetOffersByFilterQuery
+            {
+                Search = filterDto.Search,
+                Category = filterDto.Category,
+                PriceFrom = filterDto.PriceFrom,
+                PriceTo = filterDto.PriceTo,
+                Sorting = filterDto.Sorting
+            };
+
+            var result = await _mediator.Send(request);
+            return Ok(_mapper.Map<IEnumerable<OfferDetailsDto>>(result));
         }
 
         [HttpGet("{id}")]
@@ -71,7 +84,7 @@ namespace musingo_backend.Controllers
             offer.ImageUrl = offerUpdateDto.ImageUrl;
             if (Enum.TryParse<OfferStatus>(offerUpdateDto.OfferStatus, out var status)) offer.OfferStatus = status;
             else return BadRequest();
-            if(Enum.TryParse<ItemCategory>(offerUpdateDto.ItemCategory, out var category)) offer.ItemCategory = category;
+            if (Enum.TryParse<ItemCategory>(offerUpdateDto.ItemCategory, out var category)) offer.ItemCategory = category;
             else return BadRequest();
 
             offer.Description = offerUpdateDto.Description;
@@ -80,6 +93,6 @@ namespace musingo_backend.Controllers
             return Ok(result);
         }
 
-       
+
     }
 }
---
+++ musingo-backend/Handlers/GetOffersByFilterHandler.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using musingo_backend.Dtos;
+using musingo_backend.Models;
+using musingo_backend.Queries;
+using musingo_backend.Repositories;
+
+namespace musingo_backend.Handlers;
+
+public class GetOffersByFilterHandler : IRequestHandler<GetOffersByFilterQuery,ICollection<Offer>?>
+{
+    private readonly IOfferRepository _offerRepository;
+
+    public GetOffersByFilterHandler(IOfferRepository offerRepository)
+    {
+        _offerRepository = offerRepository;
+    }
+    public async Task<ICollection<Offer>?> Handle(GetOffersByFilterQuery request, CancellationToken cancellationToken)
+    {
+        var offers = await _offerRepository.GetAllOffers();
+
+        if (!string.IsNullOrWhiteSpace(request.Search))
+        {
+            offers = offers.Where(x => x.Title.Contains(request.Search)).ToList();
+        }
+
+        if (!string.IsNullOrWhiteSpace(request.Category))
+            offers = offers.Where(x => x.ItemCategory == Enum.Parse<ItemCategory>(request.Category)).ToList();
+
+        if (request.PriceFrom is not null)
+            offers = offers.Where(x => x.Cost >= request.PriceFrom).ToList();
+
+        if (request.PriceTo is not null)
+            offers = offers.Where(x => x.Cost <= request.PriceTo).ToList();
+
+        var result = request.Sorting switch
+        {
+            nameof(Sorting.Latest) => offers.OrderByDescending(x => x.CreateTime),
+            nameof(Sorting.Oldest) => offers.OrderBy(x => x.CreateTime),
+            nameof(Sorting.Ascending) => offers.OrderBy(x => x.Cost),
+            nameof(Sorting.Descending) => offers.OrderBy(x => x.Cost),
+        };
+        return result.ToList();
+    }
+}
