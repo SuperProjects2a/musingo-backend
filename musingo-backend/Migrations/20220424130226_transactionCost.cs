873dfbe0f50060e8c669e5ce432e6402f64b5066
rtrydev <r.kulka44@gmail.com>
2022-04-24 15:02:40 +0200

    transaction cost migration

---
+++ musingo-backend/Migrations/20220424130226_transactionCost.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace musingo_backend.Migrations
+{
+    public partial class transactionCost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WalletBalance",
+                table: "users",
+                newName: "wallet_balance");
+
+            migrationBuilder.AddColumn<double>(
+                name: "cost",
+                table: "transactions",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "cost",
+                table: "transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "wallet_balance",
+                table: "users",
+                newName: "WalletBalance");
+        }
+    }
+}
