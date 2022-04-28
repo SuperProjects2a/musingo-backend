96cb05f233b3d4b7ff6f3d71caf4b73ff98cdbb4
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2022-04-28 14:25:30 +0200

    mmend
    Role have now default value

---
+++ musingo-backend/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using musingo_backend.Dtos;
+using musingo_backend.Repositories;
+
+namespace musingo_backend.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public AdminController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+    }
+}
---
+++ musingo-backend/Migrations/20220428122351_Role-default-value.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace musingo_backend.Migrations
+{
+    public partial class Roledefaultvalue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Role",
+                table: "users",
+                newName: "role");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "role",
+                table: "users",
+                type: "int",
+                nullable: false,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "role",
+                table: "users",
+                newName: "Role");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Role",
+                table: "users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 1);
+        }
+    }
+}
