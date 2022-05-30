218260d426e390358de3f87f9b0d1ca98f284e30
rtrydev <r.kulka44@gmail.com>
2022-05-30 19:03:04 +0200

    add id to transaction details

--- musingo-backend/Dtos/TransactionDetailsDto.cs
+++ musingo-backend/Dtos/TransactionDetailsDto.cs
@@ -2,6 +2,7 @@ namespace musingo_backend.Dtos;
 
 public class TransactionDetailsDto
 {
+    public int Id { get; set; }
     public OfferDto? Offer { get; set; }
     public UserDto? Seller { get; set; }
     public UserDto? Buyer { get; set; }
